201be6a944681cff528ed76db5d8da1776718155
niloyansella <nilo.sellathurai@student.rocva.nl>
2022-06-22 10:32:26 +0200

    Versie 21

--- public/api.js
+++ public/api.js
@@ -1,7 +1,7 @@
 const api = async (method, path, body) => {
   const response = await axios({
     method,
-    url: `https://npfinanceejs-nl.onrender.com/${path}`,
+    url: `http://localhost:7000${path}`,
     data: body,
   });
 
